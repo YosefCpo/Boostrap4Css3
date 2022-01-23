c8ee86299fd120fc6b671e94ea040c5236457d85
YosefCpo <cpo.yosef@yahoo.com>
2022-01-23 21:16:44 +0200

    Fourth Phase

--- js/main.js
+++ js/main.js
@@ -66,6 +66,21 @@ document.querySelectorAll(".second-box .ques").forEach((ques) => {
 		if (ques.children[2].checked) {
 			ques.children[0].style.width = "100%";
 			secondChecked = true;
+			setTimeout(() => {
+				document.getElementsByClassName("second-box")[0].style.opacity = "0";
+				setTimeout(() => {
+					document.getElementsByClassName("second-box")[0].style.display =
+						"none";
+					document.getElementsByClassName("third-box")[0].style.display =
+						"block";
+					document.getElementsByClassName("third-box")[0].style.opacity = "1";
+					document.getElementsByClassName("wrapper")[0].style.minHeight =
+						"110vh";
+					document.getElementsByClassName(
+						"wrapper-overlay"
+					)[0].style.minHeight = "110vh";
+				}, 500);
+			}, 1000);
 		}
 		if (secondChecked === true) {
 			document.querySelectorAll(".second-box .ques").forEach((ques) => {
@@ -79,4 +94,38 @@ document.querySelectorAll(".second-box .ques").forEach((ques) => {
 			});
 		}
 	});
+});
+
+let thirdCheck = false;
+
+document.querySelectorAll(".third-box .ques").forEach((ques) => {
+	ques.addEventListener("click", (e) => {
+		if (ques.children[2].checked) {
+			ques.children[0].style.width = "100%";
+			thirdCheck = true;
+		}
+		if (thirdCheck === true) {
+			document.querySelectorAll(".third-box .ques").forEach((ques) => {
+				if (!ques.children[2].checked) {
+					ques.style.transform = "translateX(200px)";
+					ques.style.opacity = "0.1";
+					setTimeout(() => {
+						ques.style.opacity = "0";
+					}, 1000);
+				}
+			});
+		}
+	});
+});
+
+document.getElementById("third-back").addEventListener("click", (e) => {
+	document.getElementsByClassName("third-box")[0].style.opacity = "0";
+	setTimeout(() => {
+		document.getElementsByClassName("third-box")[0].style.display = "none";
+		document.getElementsByClassName("second-box")[0].style.display = "block";
+		document.getElementsByClassName("second-box")[0].style.opacity = "1";
+		document.getElementsByClassName("wrapper")[0].style.minHeight = "200vh";
+		document.getElementsByClassName("wrapper-overlay")[0].style.minHeight =
+			"200vh";
+	}, 500);
 });
