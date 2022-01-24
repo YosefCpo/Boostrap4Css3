e27b3caf016929b18892f5e929ab509ef27f2efe
YosefCpo <cpo.yosef@yahoo.com>
2022-01-24 10:56:01 +0200

    Fix Scroll Again

--- js/main.js
+++ js/main.js
@@ -16,9 +16,15 @@ document.getElementById("back").addEventListener("click", (e) => {
 		document.getElementsByClassName("first-box")[0].style.display = "none";
 		document.getElementsByClassName("main-box")[0].style.display = "block";
 		document.getElementsByClassName("main-box")[0].style.opacity = "1";
-		document.getElementsByClassName("wrapper")[0].style.minHeight = "100vh";
-		document.getElementsByClassName("wrapper-overlay")[0].style.minHeight =
+		document.getElementsByClassName("wrapper")[0].style.height = "100vh";
+		document.getElementsByClassName("wrapper-overlay")[0].style.height =
 			"100vh";
+		// document
+		// 	.getElementsByClassName("wrapper")[0]
+		// 	.style.removeProperty("height");
+		// document
+		// 	.getElementsByClassName("wrapper-overlay")[0]
+		// 	.style.removeProperty("height");
 	}, 500);
 });
 
@@ -44,6 +50,9 @@ document.getElementById("first-next").addEventListener("click", (e) => {
 		document.getElementsByClassName("wrapper")[0].style.minHeight = "100vh";
 		document.getElementsByClassName("wrapper-overlay")[0].style.minHeight =
 			"100vh";
+		document.getElementsByClassName("wrapper")[0].style.height = "100vh";
+		document.getElementsByClassName("wrapper-overlay")[0].style.height =
+			"100vh";
 	}, 500);
 });
 
@@ -53,8 +62,8 @@ document.getElementById("second-back").addEventListener("click", (e) => {
 		document.getElementsByClassName("second-box")[0].style.display = "none";
 		document.getElementsByClassName("first-box")[0].style.display = "block";
 		document.getElementsByClassName("first-box")[0].style.opacity = "1";
-		document.getElementsByClassName("wrapper")[0].style.minHeight = "200vh";
-		document.getElementsByClassName("wrapper-overlay")[0].style.minHeight =
+		document.getElementsByClassName("wrapper")[0].style.height = "200vh";
+		document.getElementsByClassName("wrapper-overlay")[0].style.height =
 			"200vh";
 	}, 500);
 });
@@ -79,6 +88,12 @@ document.querySelectorAll(".second-box .ques").forEach((ques) => {
 					document.getElementsByClassName(
 						"wrapper-overlay"
 					)[0].style.minHeight = "100vh";
+					document.getElementsByClassName(
+						"wrapper-overlay"
+					)[0].style.minHeight = "100vh";
+					document.getElementsByClassName("wrapper")[0].style.height = "100vh";
+					document.getElementsByClassName("wrapper-overlay")[0].style.height =
+						"100vh";
 				}, 500);
 			}, 1000);
 		}
@@ -124,8 +139,5 @@ document.getElementById("third-back").addEventListener("click", (e) => {
 		document.getElementsByClassName("third-box")[0].style.display = "none";
 		document.getElementsByClassName("second-box")[0].style.display = "block";
 		document.getElementsByClassName("second-box")[0].style.opacity = "1";
-		document.getElementsByClassName("wrapper")[0].style.minHeight = "100vh";
-		document.getElementsByClassName("wrapper-overlay")[0].style.minHeight =
-			"100vh";
 	}, 500);
 });
